4b7ffec9985c101587b1f6a65dde186cf965ab39
Priyanshi Yadav <ypriyanshi872@gmail.com>
2025-09-15 01:30:59 +0530

    Time: 1489 ms (26.04%) | Memory: 55.8 MB (69.36%) - LeetSync

--- 398-random-pick-index/random-pick-index.java
+++ 398-random-pick-index/random-pick-index.java
@@ -4,11 +4,13 @@ class Solution {
     int[] nums;
     Random rand;
 
+    // Constructor (LeetCode 398 - Random Pick Index)
     public Solution(int[] nums) {
         this.nums = nums;
         this.rand = new Random();
     }
 
+    // Random Pick Index using Reservoir Sampling
     public int pick(int target) {
         int result = -1;
         int count = 0;
@@ -16,12 +18,23 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) {
                 count++;
-                // probability 1/count se current index ko pick karenge
-                if (rand.nextInt(count) == 0) {
+                if (rand.nextInt(count) == 0) { // 1/count probability
                     result = i;
                 }
             }
         }
         return result;
+    }
+
+    // Find the Difference (LeetCode 389)
+    public char findTheDifference(String s, String t) {
+        char xor = 0;
+        for (char c : s.toCharArray()) {
+            xor ^= c;
+        }
+        for (char c : t.toCharArray()) {
+            xor ^= c;
+        }
+        return xor;
     }
 }
