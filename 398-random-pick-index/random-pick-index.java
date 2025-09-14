fadc4f54eebc1fd199ee2dc0886a19b304848c55
Priyanshi Yadav <ypriyanshi872@gmail.com>
2025-09-15 01:29:51 +0530

    Time: 1513 ms (8.94%) | Memory: 56 MB (52.93%) - LeetSync

---
+++ 398-random-pick-index/random-pick-index.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    int[] nums;
+    Random rand;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rand = new Random();
+    }
+
+    public int pick(int target) {
+        int result = -1;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                count++;
+                // probability 1/count se current index ko pick karenge
+                if (rand.nextInt(count) == 0) {
+                    result = i;
+                }
+            }
+        }
+        return result;
+    }
+}
