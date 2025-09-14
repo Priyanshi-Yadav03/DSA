b0240a9e2cd25ff1c8e04712ed98300efbb44029
Priyanshi Yadav <ypriyanshi872@gmail.com>
2025-09-15 01:34:48 +0530

    Time: 73 ms (92.37%) | Memory: 56.3 MB (35.24%) - LeetSync

--- 398-random-pick-index/random-pick-index.java
+++ 398-random-pick-index/random-pick-index.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 class Solution {
     Map<Integer, List<Integer>> map;
-    Random rand;
+    Random random;
 
     // Constructor: preprocess indices of each number
     public Solution(int[] nums) {
         map = new HashMap<>();
-        rand = new Random();
+        random = new Random();
 
         for (int i = 0; i < nums.length; i++) {
             map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
@@ -17,6 +17,6 @@ class Solution {
     // Pick random index of target in O(1)
     public int pick(int target) {
         List<Integer> indices = map.get(target);
-        return indices.get(rand.nextInt(indices.size()));
+        return indices.get(random.nextInt(indices.size()));
     }
 }
